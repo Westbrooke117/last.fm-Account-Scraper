2a97769cb254669c72ab6db8d7a36b2048e6f20d
kyhaa <kyhaanderson@gmail.com>
2023-05-22 01:18:53 +1000

    Changed program to store output in .csv file instead of requiring MySQL database. Also changed date output to YYYY-MM-DD format.

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-const mysql = require('mysql');
-const cors = require('cors');
 const express = require('express');
+const { appendFileSync } = require("fs")
 const axios = require("axios");
 const rateLimit = require('axios-rate-limit');
 const {response} = require("express");
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "usbw",
-    database: "lastfmscraper",
-});
-
-class lastfm_user {
+class User {
     constructor(response) {
         this.name = response.name;
         this.country = response.country;
-        this.registered = new Date(response.registered.unixtime * 1000);
+        this.registered = convertDateToYearMonthDay(response.registered.unixtime*1000)
         this.playcount = response.playcount;
         this.artist_count = response.artist_count;
         this.album_count = response.album_count;
         this.track_count = response.track_count;
         this.subscriber = parseInt(response.subscriber);
     }
+    saveAsCSV() {
+        const csv = `${this.name},${this.country},${this.registered},${this.playcount},${this.artist_count},${this.album_count},${this.track_count},${this.subscriber}\n`;
+        try {
+            appendFileSync("scraped_data.csv", csv);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 }
-
-connection.connect(function(err){
-    if (err) throw err;
-    console.log(`Connected to database`);
-    apiIteration();
-});
 
 const api = rateLimit(axios.create(), {
     maxRequests: 10, // Limit to one request per second
@@ -40,14 +32,9 @@ const api = rateLimit(axios.create(), {
 });
 
 let currentUser = "rj"
-let numberOfUsersInDB;
 
+apiIteration();
 async function apiIteration() {
-    connection.query(`SELECT COUNT(name) FROM users`, (err, result) => {
-        if (err) throw err;
-        numberOfUsersInDB = result[0]['COUNT(name)'];
-    })
-
     let stack = [currentUser];
     let completedUsers = [currentUser];
 
@@ -78,21 +65,9 @@ async function apiIteration() {
 function userGetInfo(user){
     api.get(`https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${user.name}&api_key=82d112e473f59ade0157abe4a47d4eb5&format=json`)
         .then(function (response){
-            const user = new lastfm_user(response.data.user)
-
-            connection.query(`SELECT * FROM users WHERE name = "${user.name}"`, (err, result) => {
-                if (err) throw err;
-
-                if (result.length === 0){
-                    connection.query(`INSERT INTO users (name, country, registered, playcount, artist_count, album_count, track_count, subscriber) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, [user.name, user.country, user.registered, user.playcount, user.artist_count, user.album_count, user.track_count, user.subscriber], (err) => {
-                        if (err) throw err;
-                        console.log(`[${numberOfUsersInDB   }] Successfully inserted "${user.name}" into database`)
-                        numberOfUsersInDB++
-                    });
-                } else {
-                    // Skip duplicate user
-                }
-            });
+            const user = new User(response.data.user)
+            user.saveAsCSV()
+            console.log(`Successfully inserted "${user.name}"`)
         })
         .catch(function (error) {
             if(error.response.data.error !== 6)
@@ -104,3 +79,13 @@ function userGetInfo(user){
 app.listen(8000, () => {
     console.log('Server running at http://localhost:8000');
 });
+
+function convertDateToYearMonthDay(unix){
+    const date = new Date(unix);
+
+    const year = date.toLocaleString("default", { year: "numeric" });
+    const month = date.toLocaleString("default", { month: "2-digit" });
+    const day = date.toLocaleString("default", { day: "2-digit" });
+
+    return year + "-" + month + "-" + day;
+}
